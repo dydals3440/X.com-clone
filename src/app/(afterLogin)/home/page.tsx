a550bacf3f8666a8d211071a44dd65059ebe54a7
김용민 <dydals3440@gmail.com>
2024-06-20 20:26:16 +0900

    feat: Post 컴포넌트 생성

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,3 +1,4 @@
+import Post from '../_component/Post';
 import PostForm from './_component/PostForm';
 import Tab from './_component/Tab';
 import TabProvider from './_component/TabProvider';
@@ -9,11 +10,11 @@ export default function Home() {
 			<TabProvider>
 				<Tab />
 				<PostForm />
-				{/* <Post />
 				<Post />
 				<Post />
 				<Post />
-				<Post /> */}
+				<Post />
+				<Post />
 			</TabProvider>
 		</main>
 	);
