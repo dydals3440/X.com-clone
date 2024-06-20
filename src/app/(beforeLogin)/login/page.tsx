c3d3890e0443e4df2777efa25edf46ba57f0103f
김용민 <dydals3440@gmail.com>
2024-06-20 15:32:48 +0900

    fix: /login 경로로 이동시, 뒷 배경안 뜨는 현상 클라이언트 사이드 코드로 변경

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,5 +1,28 @@
-import { redirect } from 'next/navigation';
+'use client';
+
+import { useRouter } from 'next/navigation';
+import Main from '../_component/Main';
+
+// login => i/flow/login으로 넘어가게 하고 싶은데, 서버쪽이기 떄문에 클라이언트에서
+// 동작 X
+// import { redirect } from 'next/navigation';
+
+// export default function Login() {
+// 	redirect('/i/flow/login');
+// }
 
 export default function Login() {
-	redirect('/i/flow/login');
+	const router = useRouter();
+	router.replace('/i/flow/login');
+	// /login 페이지로 가면, 빈 화면이 나올 꺼임.
+	// 배경화면을 만들어줘야함.
+	return <Main />;
 }
+
+// router.push
+// localhost:3001/login -> localhost:3001/i/flow/login
+// 뒤로 가기시 login으로 감
+
+// router.replace
+// localhost:3001/login -> localhost:3001/i/flow/login
+// replace는 뒤로가기하면 login 이전 꺼 히스토리가 안남음.
