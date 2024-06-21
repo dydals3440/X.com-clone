d33d36ecc3f0d3f45e5df5d95a628bc886161b99
김용민 <dydals3440@gmail.com>
2024-06-21 14:08:07 +0900

    feat: 게시글 상세 페이지

--- src/app/(afterLogin)/_component/Post.tsx
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -4,12 +4,14 @@ import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import 'dayjs/locale/ko';
 import ActionButtons from './ActionButton';
+import PostArticle from './PostArticle';
 
 dayjs.extend(relativeTime);
 dayjs.locale('ko');
 
 export default function Post() {
 	const target = {
+		postId: 1,
 		User: {
 			id: 'elonmusk',
 			nickname: 'Elon Musk',
@@ -21,31 +23,35 @@ export default function Post() {
 	};
 
 	return (
-		<article className={style.post}>
-			<div className={style.postWrapper}>
-				<div className={style.postUserSection}>
-					<Link href={`/${target.User.id}`} className={style.postUserImage}>
-						<img src={target.User.image} alt={target.User.nickname} />
-						<div className={style.postShade} />
-					</Link>
-				</div>
-				<div className={style.postBody}>
-					<div className={style.postMeta}>
-						<Link href={`/${target.User.id}`}>
-							<span className={style.postUserName}>{target.User.nickname}</span>
-							&nbsp;
-							<span className={style.postUserId}>@{target.User.id}</span>
-							&nbsp; · &nbsp;
+		<PostArticle post={target}>
+			<article className={style.post}>
+				<div className={style.postWrapper}>
+					<div className={style.postUserSection}>
+						<Link href={`/${target.User.id}`} className={style.postUserImage}>
+							<img src={target.User.image} alt={target.User.nickname} />
+							<div className={style.postShade} />
 						</Link>
-						<span className={style.postDate}>
-							{dayjs(target.createdAt).fromNow(true)}
-						</span>
 					</div>
-					<div>{target.content}</div>
-					<div className={style.postImageSection}></div>
-					<ActionButtons />
+					<div className={style.postBody}>
+						<div className={style.postMeta}>
+							<Link href={`/${target.User.id}`}>
+								<span className={style.postUserName}>
+									{target.User.nickname}
+								</span>
+								&nbsp;
+								<span className={style.postUserId}>@{target.User.id}</span>
+								&nbsp; · &nbsp;
+							</Link>
+							<span className={style.postDate}>
+								{dayjs(target.createdAt).fromNow(true)}
+							</span>
+						</div>
+						<div>{target.content}</div>
+						<div className={style.postImageSection}></div>
+						<ActionButtons />
+					</div>
 				</div>
-			</div>
-		</article>
+			</article>
+		</PostArticle>
 	);
 }
