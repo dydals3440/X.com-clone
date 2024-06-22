5419c96dca8f5912a0f42bfc61f8008bd4360d06
김용민 <dydals3440@gmail.com>
2024-06-22 20:57:40 +0900

    feat: Chatting UI 구현

--- src/app/(afterLogin)/messages/page.tsx
+++ src/app/(afterLogin)/messages/page.tsx
@@ -1,3 +1,14 @@
+import { faker } from '@faker-js/faker';
+import style from './message.module.css';
+import Room from './_component/Room';
+
 export default function Home() {
-	return <main>쪽지페이지</main>;
+	return (
+		<main className={style.main}>
+			<div className={style.header}>
+				<h3>쪽지</h3>
+			</div>
+			<Room />
+		</main>
+	);
 }
