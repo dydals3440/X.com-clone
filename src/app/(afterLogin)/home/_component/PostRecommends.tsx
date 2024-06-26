450045fdd2d4700628ae3836ed4def7b67879a40
김용민 <dydals3440@gmail.com>
2024-06-26 17:02:07 +0900

    feat: react-query를 쓰는 이유 fresh, stale, inactive

--- src/app/(afterLogin)/home/_component/PostRecommends.tsx
+++ src/app/(afterLogin)/home/_component/PostRecommends.tsx
@@ -10,6 +10,15 @@ export default function PostRecommends() {
 	const { data } = useQuery<IPost[]>({
 		queryKey: ['posts', 'recommends'],
 		queryFn: getPostRecommends,
+		// 1분동안은 항상 Fresh한 상태
+		staleTime: 60 * 1000,
+		// inactive 보는 화면에서, postRecommends를 안쓴다, 그러면 inActive가 된다.
+		// inactive 상태일 떄 gcTime이 돌아감, 5분 뒤에는 캐시에서 매번 데이터를 불러옴
+		// 5분이 지나면 캐시가 날라가기에 새로 불러와야함.
+		// Stale Time < GcTime (s:5분, gc:3분 5분동안에는 같은 캐시데이터를 새로 불러오지 않고 재사용 하기 위함)
+		// inactive시 gc타임이 돔, inactive일떄는 3분뒤에 캐시가 사라짐
+		// staleTime을 5분으로 해놓은, 이유가 퇴색됨.
+		gcTime: 30 * 1000,
 	});
 
 	return data?.map(post => <Post key={post.postId} post={post} />);
--- src/app/_component/RQProvider.tsx
+++ src/app/_component/RQProvider.tsx
@@ -14,7 +14,15 @@ export function RQProvider({ children }: Props) {
 			defaultOptions: {
 				// react-query 전역 설정
 				queries: {
+					// stale된 데이터는 아래 3가지의 경우에 다시 가져옴.
+					// 다른 탭 갔다가 z.com으로 돌아올떄 탭전환시
 					refetchOnWindowFocus: false,
+					// 컴포넌트가 unmount됐다가, 다시 mount 됐을 경우
+					// 페이지를 이동했던지, 컴포넌트가 스테이트때문에 언마운트 됐다가, 다시 마운트 되는경우
+					retryOnMount: true,
+					// 인터넷 연결이 끊겼다가 다시 접속이 되는 경우
+					refetchOnReconnect: false,
+					// 데이터를 가져올떄 몇번정도 재시도 할 수 있는 옵션.
 					retry: false,
 				},
 			},
