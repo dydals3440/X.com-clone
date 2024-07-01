5af864a70eeb46e283e938c366c8e168bb72f118
김용민 <dydals3440@gmail.com>
2024-07-01 12:07:42 +0900

    feat: 해시태그 적용

--- src/app/(afterLogin)/_component/Trend.tsx
+++ src/app/(afterLogin)/_component/Trend.tsx
@@ -6,8 +6,11 @@ type Props = { trend: Hashtag };
 
 export default function Trend({ trend }: Props) {
 	return (
-		<Link href={`/search?q=${trend.title}`} className={style.container}>
-			<div className={style.count}>실시간트렌드</div>
+		<Link
+			href={`/search?q=${encodeURIComponent(trend.title)}`}
+			className={style.container}
+		>
+			<div className={style.count}>실시간 트렌드</div>
 			<div className={style.title}>{trend.title}</div>
 			{/* toLocaleString()하면 1000자리 넘어갈떄마다 알아서 , 찍힘 */}
 			<div className={style.count}>{trend.count.toLocaleString()} posts</div>
